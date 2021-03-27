89099eaf5a526cc13a51614a400b17980ce30b11
ThunderStormOwl <lausrock@gmail.com>
2021-03-27 11:25:13 -0300

    configured routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,2 +1,8 @@
+import { SignUpController } from './../controllers/sign-up/SignUpController';
 import {Router} from 'express';
 
+const routes = Router();
+
+routes.get('/sign-up', SignUpController.signUp);
+
+export {routes};
