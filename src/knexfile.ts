0e8a38cf72a426057f39ddd579fb5f0501a4e349
ThunderStormOwl <lausrock@gmail.com>
2021-04-17 12:05:03 -0300

    error fix on tsc command

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -12,6 +12,9 @@ const development = {
 
 const production = {
     client: 'pg',
+    migrations: {
+        directory: path.resolve(__dirname, 'database', 'migrations')
+    },
     connection: {
         port: Number(process.env.DATABASE_PORT || 5432),
         host: process.env.DATABASE_HOST,
@@ -19,8 +22,8 @@ const production = {
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
         ssl: process.env.DATABASE_SSL ? 
-            {rejectUnauthorized : process.env.DATABASE_SSL_REJCT_UNAUTHORIZED === 'false' ? false: true} 
-            : undefined
+            {rejectUnauthorized : process.env.DATABASE_SSL_REJECT_UNAUTHORIZED === 'false' ? false: true} 
+            : undefined,
     }
 }
 
